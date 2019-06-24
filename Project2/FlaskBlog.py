716bb4b3548c97b23d1536dfaa1d29d1dc95dec1
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-06-25 01:31:42 +0200

    feature: Using template inheritance add home and about pages

--- Project2/FlaskBlog.py
+++ Project2/FlaskBlog.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Mohamed Mamdouh',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 18, 2019'
+    },
+    {
+        'author': 'Tarek Mamdouh',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 05, 2018'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "Home Page"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "About Page"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
