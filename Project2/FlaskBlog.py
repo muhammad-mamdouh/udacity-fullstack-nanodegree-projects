3bffcbbc670e44e47ec68788293115e06f798621
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-06-25 01:15:55 +0200

    feature: Add the main flask app script with routes for /home and /about pages

---
+++ Project2/FlaskBlog.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return "Home Page"
+
+
+@app.route("/about")
+def about():
+    return "About Page"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run('', 5000)
