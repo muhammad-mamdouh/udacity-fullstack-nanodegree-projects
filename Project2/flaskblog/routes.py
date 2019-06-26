879a7d03aca37d31735e4e3c728e94d2bc07ab5b
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-06-26 07:39:54 +0200

    feature: Resize the updated profile pic before being saved

--- Project2/flaskblog/routes.py
+++ Project2/flaskblog/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -74,7 +75,11 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
 
     return picture_fn
 
