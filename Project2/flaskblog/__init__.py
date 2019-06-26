d33ca94472e9fa3659c2e8c8a8ecaeed49fa52e5
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-06-26 05:31:15 +0200

    feature: Add login required restriction to the newly defined account route

--- Project2/flaskblog/__init__.py
+++ Project2/flaskblog/__init__.py
@@ -9,5 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)    # this is a database sqlalchemy instance
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
--- Project2/flaskblog/routes.py
+++ Project2/flaskblog/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -55,7 +55,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash(f'Login Unsuccessful! Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -64,3 +65,9 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
