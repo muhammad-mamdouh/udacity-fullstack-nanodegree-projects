a047d873212bf7f10b2e5b336c0d9e9e77f90b1b
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-06-26 13:59:04 +0200

    feature: Add pagination for the home page posts

--- Project2/flaskblog/routes.py
+++ Project2/flaskblog/routes.py
@@ -11,7 +11,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
