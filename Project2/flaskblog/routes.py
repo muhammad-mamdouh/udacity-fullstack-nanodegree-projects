9e366607603699190afa14ed5f10753c8cce8ea2
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-06-26 14:03:36 +0200

    feature: Display posts by the latest ones

--- Project2/flaskblog/routes.py
+++ Project2/flaskblog/routes.py
@@ -12,7 +12,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/home")
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.paginate(page=page, per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
