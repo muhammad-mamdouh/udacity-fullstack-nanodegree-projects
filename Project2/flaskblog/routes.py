ef7cbb47bcb90bdfab9c1e29c1cb72e62327e263
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-06-26 19:48:48 +0200

    feature: Display all of the posts created by a specific user

--- Project2/flaskblog/routes.py
+++ Project2/flaskblog/routes.py
@@ -142,3 +142,13 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+            .order_by(Post.date_posted.desc())\
+            .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
