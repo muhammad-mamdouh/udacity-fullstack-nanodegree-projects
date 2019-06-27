fe758f5647ab65b5052de5a4357442273ab55892
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-06-27 09:28:00 +0200

    feature: Move app configs into her own class

--- Project2/flaskblog/__init__.py
+++ Project2/flaskblog/__init__.py
@@ -1,24 +1,18 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ec14e62f6d91f3231b5b99bcd7d61836'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)    # this is a database sqlalchemy instance
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('DEFAULT_SENDER_NAME')
+
 mail = Mail(app)
 
 from flaskblog.users.routes import users
