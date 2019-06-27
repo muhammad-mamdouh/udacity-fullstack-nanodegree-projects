14e76accae3b04b62b28249c4f252ad20f8d8240
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-06-27 06:07:24 +0200

    feature: Add password reset feature by their gmail email

--- Project2/flaskblog/__init__.py
+++ Project2/flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ec14e62f6d91f3231b5b99bcd7d61836'
@@ -11,5 +13,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('DEFAULT_SENDER_NAME')
+mail = Mail(app)
 
 from flaskblog import routes
