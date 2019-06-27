0a9093058bf288bb0ec736cb118a6891110e952b
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-06-28 00:06:06 +0200

    feature: Change the environment variables to a json dictionary

--- Project2/flaskblog/config.py
+++ Project2/flaskblog/config.py
@@ -1,12 +1,16 @@
 import os
+import json
+
+with open('/etc/flask_blog_config.json') as config_file:
+    config = json.load(config_file)
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-    MAIL_DEFAULT_SENDER = os.environ.get('DEFAULT_SENDER_NAME')
+    MAIL_USERNAME = config.get('EMAIL_USER')
+    MAIL_PASSWORD = config.get('EMAIL_PASS')
+    MAIL_DEFAULT_SENDER = config.get('DEFAULT_SENDER_NAME')
