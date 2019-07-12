d48cb2bbc5ce5edc01d0a1c7bedb3f6458bd0e35
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-12 22:00:57 +0200

    feature: Add the layout template for both home and about pages using template inheritance

--- Project3/ItemCatalog.py
+++ Project3/ItemCatalog.py
@@ -1,17 +1,36 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+
+
+items = [
+    {
+        'author': 'Mohamed Mamdouh',
+        'title': 'Snowboard',
+        'description': 'Description for the first item.',
+        'category': 'Soccer',
+        'date_published': 'May 18, 2019'
+    },
+    {
+        'author': 'Tarek Mamdouh',
+        'title': 'Stick',
+        'description': 'Description for the second item.',
+        'category': 'Hockey',
+        'date_published': 'May 05, 2019'
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Home Page"
+    return render_template('home.html', items=items)
 
 
 @app.route('/about')
 def about():
-    return "About Page"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
