1ee20b5bb990d6d1f6bfbe0bbe8c95d42bc56cf5
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-13 12:57:51 +0200

    feature: Add register and login routes

--- Project3/ItemCatalog.py
+++ Project3/ItemCatalog.py
@@ -1,8 +1,9 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+import secrets
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 items = [
     {
@@ -30,7 +31,28 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check your username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
