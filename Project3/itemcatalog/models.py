7cb3d94410bf2e40ba47cd6c054631b909f6c44f
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-13 20:10:25 +0200

    feature: Add database models for the user, category and item tables

---
+++ Project3/itemcatalog/models.py
@@ -0,0 +1,38 @@
+from itemcatalog import db
+from datetime import datetime
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    items = db.relationship('Item', backref='item_author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    items = db.relationship('Item', backref='item_category', cascade="all, delete-orphan", lazy=True)
+
+    def __repr__(self):
+        return f"Category('{self.name}')"
+
+
+class Item(db.Model):
+    __tablename__ = 'item'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date_published = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Item('{self.name}, {self.date_published}, {self.item_author.username}, {self.item_category.name}')"
