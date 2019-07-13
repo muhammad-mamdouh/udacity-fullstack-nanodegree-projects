224cf01b71360aeefa8385b9685283cfd515f390
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-13 21:21:12 +0200

    feature: Add the login_manager instance to manage the login sessions

--- Project3/itemcatalog/__init__.py
+++ Project3/itemcatalog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import secrets
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ app.config['SECRET_KEY'] = secrets.token_hex(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from itemcatalog import routes
--- Project3/itemcatalog/models.py
+++ Project3/itemcatalog/models.py
@@ -1,8 +1,14 @@
-from itemcatalog import db
+from itemcatalog import db, login_manager
+from flask_login import UserMixin
 from datetime import datetime
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
