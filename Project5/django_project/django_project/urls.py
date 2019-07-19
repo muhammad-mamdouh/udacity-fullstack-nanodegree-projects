9a8e15de15f67b41d65c2e66fa64e702bf58e346
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-19 19:15:29 +0200

    feature: Redirect any root requests to the blog app

--- Project5/django_project/django_project/urls.py
+++ Project5/django_project/django_project/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    path('blog/about/', include('blog.urls')),
+    path('', include('blog.urls')),
 ]
