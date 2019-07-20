80500b91a8db3fd0584c0e234cbf1b249d85e161
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-20 09:07:16 +0200

    feature: Pass arguments to the templates

--- Project5/django_project/blog/views.py
+++ Project5/django_project/blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 6, 2019'
+    },
+    {
+        'author': 'M_Mamdouh',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 18, 2019'
+    }
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
