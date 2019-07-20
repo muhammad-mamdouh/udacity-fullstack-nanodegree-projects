5152c348797610a97159dbc480d806247d1bafa9
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-20 18:28:40 +0200

    feature: Retrieve posts from the DB instead of the dummy data

--- Project5/django_project/blog/views.py
+++ Project5/django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'October 6, 2019'
-    },
-    {
-        'author': 'M_Mamdouh',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'October 18, 2019'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
