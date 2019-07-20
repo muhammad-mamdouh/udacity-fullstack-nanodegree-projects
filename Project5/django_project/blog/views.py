228c5f8496ba4e63bd2f2d89196edcece6c1a95a
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-20 07:55:05 +0200

    feature: Render templates from the view functions

--- Project5/django_project/blog/views.py
+++ Project5/django_project/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
