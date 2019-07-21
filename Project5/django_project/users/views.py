af6b8d35fcfecc0574bb21af9bd72a6659f89382
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-21 19:21:32 +0200

    feature: Handle post requests for the account profile page forms

--- Project5/django_project/users/views.py
+++ Project5/django_project/users/views.py
@@ -19,8 +19,19 @@ def register(request):
 
 @login_required()
 def profile(request):
-    user_form = UserUpdateForm(instance=request.user)
-    profile_form = ProfileUpdateForm(instance=request.user.profile)
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST,
+                                         request.FILES,
+                                         instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your account has been updated!')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'user_form': user_form,
