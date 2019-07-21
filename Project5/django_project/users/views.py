12efceb9f0772c8367eb3fe90909f50f4adbb968
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-21 11:18:29 +0200

    refactor: Change register view redirect url to the login page

--- Project5/django_project/users/views.py
+++ Project5/django_project/users/views.py
@@ -9,8 +9,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! Now you are able to log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
