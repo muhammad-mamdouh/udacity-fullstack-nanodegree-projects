90d90431fb4f56d5c4e4524971d5947253c9ea4a
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-21 17:39:40 +0200

    feature: Add those media routes to the site urlpatterns

--- Project5/django_project/django_project/urls.py
+++ Project5/django_project/django_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
@@ -26,3 +28,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
