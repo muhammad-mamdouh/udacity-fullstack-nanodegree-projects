ea0c9ab5c5e87d5ff9399549ec53df34063ddbe5
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-21 11:57:54 +0200

    feature: Create account profile view

--- Project5/django_project/users/views.py
+++ Project5/django_project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -14,3 +15,8 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
