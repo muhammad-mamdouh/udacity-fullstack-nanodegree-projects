19bc96d9488e0bbb7f8e717fc0c53367345706e7
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-22 11:53:54 +0200

    refactor: Use list class-based-views instead of function-based-ones for the home page

--- Project5/django_project/blog/urls.py
+++ Project5/django_project/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import PostListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
 ]
--- Project5/django_project/blog/views.py
+++ Project5/django_project/blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'    # Default naming convention <app>/<modelname>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 
 def about(request):
